ab39012c368504772f674820d482c49ce3c98fed
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2025-01-28 21:50:26 -0500

    Add SetIconFromFilePath function
    (cherry picked from commit b00dd2a66a4b3974b8acef700f7eb463915d5e3e)

--- systray_unix.go
+++ systray_unix.go
@@ -103,6 +103,17 @@ func SetIconName(iconName string) {
 	}
 }
 
+// SetIconFromFilePath sets the systray icon from a file path.
+// iconFilePath should be the path to a .ico for windows and .ico/.jpg/.png for other platforms.
+func SetIconFromFilePath(iconFilePath string) error {
+	bytes, err := os.ReadFile(iconFilePath)
+	if err != nil {
+		return err
+	}
+	SetIcon(bytes)
+	return nil
+}
+
 // SetTitle sets the systray title, only available on Mac and Linux.
 func SetTitle(t string) {
 	instance.lock.Lock()
